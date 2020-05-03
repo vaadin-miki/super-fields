8b0a1b71a579ed8b7eefaca62abdcdf5a6280821
miki <miki@vaadin.com>
2020-05-03 23:05:12 +0300

    #85 done
    ItemGrid now implements WithItemsMixin

--- superfields/src/main/java/org/vaadin/miki/superfields/itemgrid/ItemGrid.java
+++ superfields/src/main/java/org/vaadin/miki/superfields/itemgrid/ItemGrid.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.data.binder.HasItems;
+import org.vaadin.miki.markers.WithItemsMixin;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -30,7 +31,7 @@ import java.util.stream.Stream;
  * @since 2020-04-14
  */
 @Tag("item-grid")
-public class ItemGrid<T> extends CustomField<T> implements HasItems<T>, HasStyle {
+public class ItemGrid<T> extends CustomField<T> implements HasItems<T>, HasStyle, WithItemsMixin<T, ItemGrid<T>> {
 
     /**
      * Default number of columns.
