9be98fa4666c2343c8d77339c63f51c9b49479a3
Miki <miki@vaadin.com>
2020-07-03 18:04:05 +0300

    #183 most likely done
    two new methods: one for returning the UnloadObserver attached to a given Component, one for attaching to current UI

---
+++ superfields/src/test/java/org/vaadin/miki/superfields/unload/SampleView.java
@@ -0,0 +1,13 @@
+package org.vaadin.miki.superfields.unload;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * A dummy view for testing.
+ * @author miki
+ * @since 2020-07-03
+ */
+@Route("")
+public class SampleView extends VerticalLayout {
+}
