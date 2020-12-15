f5a71a9a3784ab4c4b864590726a442a6dda7442
Miki <miki@vaadin.com>
2020-12-15 23:10:46 +0200

    #250 added RowPaddingStrategy
    this is a little buggy, as null is implicitly passed to cell generators
    also last row padding does not work due to incorrect while condition
    
    looks flexible though, so it will likely stay this way

--- demo-v14/src/main/java/org/vaadin/miki/demo/builders/ItemGridBuilder.java
+++ demo-v14/src/main/java/org/vaadin/miki/demo/builders/ItemGridBuilder.java
@@ -2,12 +2,17 @@ package org.vaadin.miki.demo.builders;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import org.vaadin.miki.demo.ContentBuilder;
 import org.vaadin.miki.demo.Order;
 import org.vaadin.miki.demo.providers.ItemGridGenerators;
 import org.vaadin.miki.superfields.itemgrid.ItemGrid;
+import org.vaadin.miki.superfields.itemgrid.RowPaddingStrategies;
+import org.vaadin.miki.superfields.itemgrid.RowPaddingStrategy;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -18,6 +23,15 @@ import java.util.function.Consumer;
 @Order(60)
 @SuppressWarnings("squid:S5411") // no way around boxed values
 public class ItemGridBuilder implements ContentBuilder<ItemGrid<?>> {
+
+    private final Map<RowPaddingStrategy, String> strategyCaptions = new LinkedHashMap<>();
+
+    public ItemGridBuilder() {
+        this.strategyCaptions.put(RowPaddingStrategies.NO_PADDING, "No padding");
+        this.strategyCaptions.put(RowPaddingStrategies.FIRST_ROW_FILL_BEGINNING, "Pad first row at beginning");
+        this.strategyCaptions.put(RowPaddingStrategies.LAST_ROW_CENTRE_BEGINNING, "Last row centred");
+        this.strategyCaptions.put(RowPaddingStrategies.LAST_ROW_FILL_END, "Pad last row at end");
+    }
 
     @Override
     public void buildContent(ItemGrid<?> component, Consumer<Component[]> callback) {
@@ -34,6 +48,13 @@ public class ItemGridBuilder implements ContentBuilder<ItemGrid<?>> {
                 )
         );
 
-        callback.accept(new Component[]{buttons, alternate});
+        final ComboBox<RowPaddingStrategy> rowPaddingStrategies = new ComboBox<>("Select row padding strategy:", this.strategyCaptions.keySet());
+        rowPaddingStrategies.setItemLabelGenerator(this.strategyCaptions::get);
+        rowPaddingStrategies.setHelperText("(row padding strategy decides where to draw empty cells, if at all)");
+        rowPaddingStrategies.setWidth("350px");
+        rowPaddingStrategies.setValue(RowPaddingStrategies.NO_PADDING);
+        rowPaddingStrategies.addValueChangeListener(event -> component.setRowPaddingStrategy(event.getValue()));
+
+        callback.accept(new Component[]{buttons, alternate, rowPaddingStrategies});
     }
 }
