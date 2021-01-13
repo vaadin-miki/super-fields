372b6ce0db96fbf4e924a50425541579f1823dd1
Miki <miki@vaadin.com>
2021-01-13 13:30:23 +0200

    superdatetimepicker type error no longer shown (#262)
    * #260 fixed
    
    * #87 also seems fixed
    
    removed demo code that showed the footnote

--- superfields/src/main/resources/META-INF/resources/frontend/super-date-time-picker.js
+++ superfields/src/main/resources/META-INF/resources/frontend/super-date-time-picker.js
@@ -10,7 +10,10 @@ class SuperDateTimePicker extends DatePatternMixin.to(DateTimePicker) {
     }
 
     setDisplayPattern(datepicker, displayPattern) {
-        return super.setDisplayPattern(datepicker.querySelector('vaadin-date-time-picker-date-picker'), displayPattern);
+        // this method may be called by inner method that changes locale when the pattern is present
+        // in such case, the datepicker object is the actual date picker, not the date-time picker container
+        // see https://github.com/vaadin-miki/super-fields/issues/260
+        return super.setDisplayPattern(datepicker.tagName === 'VAADIN-DATE-TIME-PICKER-DATE-PICKER' ? datepicker : datepicker.querySelector('vaadin-date-time-picker-date-picker'), displayPattern);
     }
 
 }
