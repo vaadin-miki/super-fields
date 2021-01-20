a47547c412d877daa4c258a1f8b29d26d48defed
Miki <miki@vaadin.com>
2021-01-20 08:35:49 +0200

    Scientific notation support for SuperBigDecimalField (#266)
    #244 done

---
+++ demo-v14/src/main/java/org/vaadin/miki/demo/builders/SuperBigDecimalFieldBuilder.java
@@ -0,0 +1,33 @@
+package org.vaadin.miki.demo.builders;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import org.vaadin.miki.demo.ContentBuilder;
+import org.vaadin.miki.demo.Order;
+import org.vaadin.miki.superfields.numbers.SuperBigDecimalField;
+
+import java.util.function.Consumer;
+
+/**
+ * Supports content for {@link SuperBigDecimalField}.
+ * @author miki
+ * @since 2021-01-15
+ */
+@Order(25)
+public class SuperBigDecimalFieldBuilder implements ContentBuilder<SuperBigDecimalField> {
+
+    @Override
+    @SuppressWarnings("squid:S5411") // no way around boxed value
+    public void buildContent(SuperBigDecimalField component, Consumer<Component[]> callback) {
+        final Checkbox allowScientificNotation = new Checkbox("Support scientific notation as input? (format: 1 + 3 e 2)?");
+        allowScientificNotation.addValueChangeListener(event -> {
+            if(event.getValue())
+                component.withMaximumExponentDigits(2)
+                        .withMaximumSignificandFractionDigits(3)
+                        .withMaximumSignificandIntegerDigits(1);
+            else
+                component.setMaximumExponentDigits(0);
+        });
+        callback.accept(new Component[]{allowScientificNotation});
+    }
+}
