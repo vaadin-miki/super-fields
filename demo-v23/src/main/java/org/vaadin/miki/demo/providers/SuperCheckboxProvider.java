4d9307ebb7c636a46417a04a66b78d591580c590
Miki <miki@vaadin.com>
2022-09-15 13:01:45 +0300

    #412 done

---
+++ demo-v23/src/main/java/org/vaadin/miki/demo/providers/SuperCheckboxProvider.java
@@ -0,0 +1,18 @@
+package org.vaadin.miki.demo.providers;
+
+import org.vaadin.miki.demo.ComponentProvider;
+import org.vaadin.miki.demo.Order;
+import org.vaadin.miki.superfields.checkbox.SuperCheckbox;
+
+/**
+ * Provides a fresh {@link SuperCheckbox}.
+ * @author miki
+ * @since 2022-09-15
+ */
+@Order(99)
+public class SuperCheckboxProvider implements ComponentProvider<SuperCheckbox> {
+    @Override
+    public SuperCheckbox getComponent() {
+        return new SuperCheckbox().withLabel("This component can be made read-only, contrary to original Checkbox.");
+    }
+}
