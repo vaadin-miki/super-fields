e6eef3ab294876aa2e5f365f30df0e6f5eaff8ee
Miki <miki@vaadin.com>
2022-09-26 11:27:07 +0300

    #414 LabelField now has the css import
    updated javadocs

--- superfields/src/main/java/org/vaadin/miki/shared/labels/LabelPosition.java
+++ superfields/src/main/java/org/vaadin/miki/shared/labels/LabelPosition.java
@@ -10,13 +10,45 @@ import java.util.Locale;
  */
 public enum LabelPosition {
 
+    /**
+     * Default label position, without any changes.
+     */
     DEFAULT(false),
+
+    /**
+     * Label is placed to the side of the component, before it, and aligned to the start of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the left, aligned to the top.
+     */
     BEFORE_START,
+    /**
+     * Label is placed to the side of the component, before it, and aligned to the middle of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the left, in the vertical middle of column.
+     */
     BEFORE_MIDDLE,
+    /**
+     * Label is placed to the side of the component, before it, and aligned to the end of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the left, aligned to the bottom.
+     */
     BEFORE_END,
+    /**
+     * Label is placed to the side of the component, after it, and aligned to the start of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the right, aligned to the top.
+     */
     AFTER_START,
+    /**
+     * Label is placed to the side of the component, after it, and aligned to the middle of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the right, in the vertical middle of column.
+     */
     AFTER_MIDDLE,
+    /**
+     * Label is placed to the side of the component, after it, and aligned to the end of the column.
+     * In left-to-right, top-to-bottom layouts, this means: label on the right, aligned to the bottom.
+     */
     AFTER_END,
+    /**
+     * Label is placed as the last thing of the entire component.
+     * In left-to-right, top-to-bottom layouts, this means: label on the bottom.
+     */
     LAST(false);
 
     private final String positionData;
@@ -25,12 +57,18 @@ public enum LabelPosition {
         this(true);
     }
 
+    /**
+     * Creates the enum.
+     * @param side Whether the label is on the side of the component.
+     */
     LabelPosition(boolean side) {
         this.positionData = String.join(" ", ((side ? "side_" : "") + this.name()).toLowerCase(Locale.ROOT).split("_"));
     }
 
     /**
      * The attribute value that corresponds to the given label position. Used by CSS selectors.
+     * This is a space-separated list of styles. Never {@code null}.
+     *
      * @return A non-{@code null} array.
      */
     public String getPositionData() {
--- superfields/src/main/java/org/vaadin/miki/superfields/text/LabelField.java
+++ superfields/src/main/java/org/vaadin/miki/superfields/text/LabelField.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.function.SerializableFunction;
 import org.vaadin.miki.markers.WithHelperMixin;
 import org.vaadin.miki.markers.WithHelperPositionableMixin;
@@ -21,6 +22,7 @@ import org.vaadin.miki.markers.WithValueMixin;
  * @author miki
  * @since 2022-04-08
  */
+@CssImport(value = "./styles/label-positions.css", themeFor = "vaadin-custom-field")
 public class LabelField<V> extends CustomField<V> implements HasStyle, WithLabelMixin<LabelField<V>>,
         WithHelperPositionableMixin<LabelField<V>>, WithHelperMixin<LabelField<V>>,
         WithLabelPositionableMixin<LabelField<V>>, WithIdMixin<LabelField<V>>,
