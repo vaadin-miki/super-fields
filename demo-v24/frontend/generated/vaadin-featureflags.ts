4b964c24cf2daabc9ef4ea7da19b9ad59a7eecf8
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2023-03-06 09:49:16 +0200

    Bump the version to Vaadin 24.rc2 Add vaadin.frontend.hotdeploy in the demo (easier to develop)

--- demo-v24/frontend/generated/vaadin-featureflags.ts
+++ demo-v24/frontend/generated/vaadin-featureflags.ts
@@ -2,9 +2,6 @@
 window.Vaadin = window.Vaadin || {};
 window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
 window.Vaadin.featureFlags.exampleFeatureFlag = false;
-window.Vaadin.featureFlags.hillaPush = false;
-window.Vaadin.featureFlags.hillaEngine = false;
 window.Vaadin.featureFlags.collaborationEngineBackend = false;
 window.Vaadin.featureFlags.enforceFieldValidation = false;
-window.Vaadin.featureFlags.expressBuild = false;
 export {};
