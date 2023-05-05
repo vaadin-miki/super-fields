298f505061a266c90f1e0734c579cf852c011520
miki <miki@beanbakers.fi>
2023-05-05 10:05:31 +0300

    #471 demo page more consistent with everything else

--- demo-v24/src/main/java/org/vaadin/miki/demo/builders/HasTextInputModeBuilder.java
+++ demo-v24/src/main/java/org/vaadin/miki/demo/builders/HasTextInputModeBuilder.java
@@ -2,6 +2,7 @@ package org.vaadin.miki.demo.builders;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Span;
 import org.vaadin.miki.demo.ContentBuilder;
 import org.vaadin.miki.demo.Order;
 import org.vaadin.miki.markers.HasTextInputMode;
@@ -14,13 +15,12 @@ import java.util.function.Consumer;
  * @author miki
  * @since 2023-04-21
  */
-@Order(200)
+@Order(119)
 public class HasTextInputModeBuilder implements ContentBuilder<HasTextInputMode> {
   @Override
   public void buildContent(HasTextInputMode component, Consumer<Component[]> callback) {
     final ComboBox<TextInputMode> modes = new ComboBox<>("Select text input mode:", TextInputMode.values());
-    modes.setHelperText("The change should affect the on-screen keyboard of your device.");
     modes.addValueChangeListener(event -> component.setTextInputMode(event.getValue()));
-    callback.accept(new Component[]{modes});
+    callback.accept(new Component[]{modes, new Span("Note: the change should affect the on-screen keyboard of your device.")});
   }
 }
