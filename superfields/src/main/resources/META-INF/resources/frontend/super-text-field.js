a737571a460075deaf8620f4202ab23bb9c70e79
miki <miki@beanbakers.fi>
2023-06-02 11:33:17 +0300

    #473 encouraging first take

--- superfields/src/main/resources/META-INF/resources/frontend/super-text-field.js
+++ superfields/src/main/resources/META-INF/resources/frontend/super-text-field.js
@@ -11,6 +11,43 @@ class SuperTextField extends TextSelectionMixin.to(TextField) {
         this.listenToEvents(this.inputElement, this, callingServer);
     }
 
+    ensureValidText(e, webComponent, textInput) {
+        if (webComponent.pattern !== undefined && webComponent.preventInvalidMixin !== undefined && webComponent.preventInvalidMixin.preventInvalidInput) {
+            if (new RegExp(webComponent.pattern).test(textInput.value)) {
+                webComponent.preventInvalidMixin.lastValue = textInput.value;
+            }
+            else {
+                textInput.value = webComponent.preventInvalidMixin.lastValue;
+                webComponent._markInputPrevented();
+            }
+        }
+        else if (webComponent.preventInvalidMixin !== undefined) {
+            webComponent.preventInvalidMixin.lastValue = textInput.value;
+        }
+    }
+
+    preventInvalidInput(prevent) {
+        console.log('STF: preventing invalid input set to ' + prevent);
+        const listener = (e) => this.ensureValidText(e, this,  this.inputElement);
+        let lastKnownValue = this.inputElement.value;
+        if (this.preventInvalidMixin === undefined) {
+            this.preventInvalidMixin = {
+                lastValue: lastKnownValue,
+                preventInvalidInput: prevent
+            };
+        }
+        else if (!new RegExp(this.inputElement.pattern).test(lastKnownValue) && prevent) {
+            this.preventInvalidMixin.lastValue = '';
+            this.preventInvalidMixin.preventInvalidInput = true;
+            this.inputElement.value = '';
+        }
+        else {
+            this.preventInvalidMixin.lastValue = lastKnownValue;
+            this.preventInvalidMixin.preventInvalidInput = prevent;
+        }
+        this.inputElement.addEventListener('input', listener);
+    }
+
 }
 
 customElements.define(SuperTextField.is, SuperTextField);
