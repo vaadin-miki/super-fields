3dc0776a9424f28dc21ad22caa79545f0d04b02d
Miki <miki@beanbakers.fi>
2023-11-10 10:41:55 +0200

    #487 done (#491)
    Vaadin upgraded to 24.2.2

--- demo-v24/frontend/generated/vaadin-featureflags.js
+++ demo-v24/frontend/generated/vaadin-featureflags.js
@@ -3,4 +3,6 @@ window.Vaadin = window.Vaadin || {};
 window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
 window.Vaadin.featureFlags.exampleFeatureFlag = false;
 window.Vaadin.featureFlags.collaborationEngineBackend = false;
+window.Vaadin.featureFlags.webPush = false;
+window.Vaadin.featureFlags.formFillerAddon = false;
 export {};
