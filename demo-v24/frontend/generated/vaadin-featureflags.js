f4455061f7e12b9e540a45b27892f152ece0b2e5
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-10-20 11:25:17 +0200

    refactor: upgrade to Vaadin 25

--- demo-v24/frontend/generated/vaadin-featureflags.js
+++ demo-v24/frontend/generated/vaadin-featureflags.js
@@ -2,15 +2,13 @@
 window.Vaadin = window.Vaadin || {};
 window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
 if (Object.keys(window.Vaadin.featureFlags).length === 0) {
-window.Vaadin.featureFlags.exampleFeatureFlag = false;
 window.Vaadin.featureFlags.collaborationEngineBackend = false;
-window.Vaadin.featureFlags.formFillerAddon = false;
-window.Vaadin.featureFlags.fullstackSignals = false;
 window.Vaadin.featureFlags.flowFullstackSignals = false;
-window.Vaadin.featureFlags.copilotExperimentalFeatures = false;
-window.Vaadin.featureFlags.masterDetailLayoutComponent = false;
-window.Vaadin.featureFlags.react19 = false;
 window.Vaadin.featureFlags.accessibleDisabledButtons = false;
+window.Vaadin.featureFlags.themeComponentStyles = false;
+window.Vaadin.featureFlags.copilotExperimentalFeatures = false;
+window.Vaadin.featureFlags.fullstackSignals = false;
+window.Vaadin.featureFlags.masterDetailLayoutComponent = false;
 window.Vaadin.featureFlags.layoutComponentImprovements = false;
 window.Vaadin.featureFlags.defaultAutoResponsiveFormLayout = false;
 };
