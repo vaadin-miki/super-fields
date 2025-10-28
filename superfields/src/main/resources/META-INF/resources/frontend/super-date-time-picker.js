c57a82869f465164e150159694fddec9d9454a2c
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2025-10-28 10:19:44 +0100

    refactor: upgrade to Vaadin 25 (#559)
    code courtesy of Sascha Ißbrücker

--- superfields/src/main/resources/META-INF/resources/frontend/super-date-time-picker.js
+++ superfields/src/main/resources/META-INF/resources/frontend/super-date-time-picker.js
@@ -6,14 +6,14 @@ class SuperDateTimePicker extends DatePatternMixin.to(DateTimePicker) {
     static get is() { return 'super-date-time-picker'; }
 
     initPatternSetting(datepicker) {
-        super.initPatternSetting(datepicker.querySelector('vaadin-date-time-picker-date-picker'));
+        super.initPatternSetting(datepicker.querySelector('vaadin-date-picker'));
     }
 
     setDisplayPattern(datepicker, displayPattern) {
         // this method may be called by inner method that changes locale when the pattern is present
         // in such case, the datepicker object is the actual date picker, not the date-time picker container
         // see https://github.com/vaadin-miki/super-fields/issues/260
-        return super.setDisplayPattern(datepicker.tagName === 'VAADIN-DATE-TIME-PICKER-DATE-PICKER' ? datepicker : datepicker.querySelector('vaadin-date-time-picker-date-picker'), displayPattern);
+        return super.setDisplayPattern(datepicker.tagName === 'VAADIN-DATE-PICKER' ? datepicker : datepicker.querySelector('vaadin-date-picker'), displayPattern);
     }
 
 }
