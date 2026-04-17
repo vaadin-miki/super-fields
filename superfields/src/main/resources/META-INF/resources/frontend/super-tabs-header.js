b96c81973b146326e312c038b4c3dce569073e71
miki <miki@beanbakers.fi>
2026-04-17 10:58:19 +0300

    #574 done

--- superfields/src/main/resources/META-INF/resources/frontend/super-tabs-header.js
+++ superfields/src/main/resources/META-INF/resources/frontend/super-tabs-header.js
@@ -1,12 +1,16 @@
-import {Tabs} from "@vaadin/tabs"
-import { tabsStyles } from '@vaadin/tabs/src/styles/vaadin-tabs-base-styles.js';
+import {Tabs} from '@vaadin/tabs';
+import {tabsStyles} from '@vaadin/tabs/src/styles/vaadin-tabs-base-styles.js';
 import {CSS_SUPER_TABS_MULTILINE} from "./styles/css-super-tabs-multiline";
 
 class SuperTabsHeader extends Tabs {
 
-    static get is() {return 'super-tabs-header'}
+    static get is() {
+        return 'super-tabs-header'
+    }
 
-    static get styles() {return [tabsStyles, CSS_SUPER_TABS_MULTILINE]}
+    static get styles() {
+        return [tabsStyles, CSS_SUPER_TABS_MULTILINE]
+    }
 
 }
 
